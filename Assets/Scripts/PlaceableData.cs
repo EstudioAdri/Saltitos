aa2f3bcdb5893a801802030a4d179cdbbe2f38d9
avallejoganga <114919382+avallejoganga@users.noreply.github.com>
2022-11-17 18:00:42 +0100

    PlaceableData

---
+++ Assets/Scripts/PlaceableData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableData : ScriptableObject
+{
+    [Header("Common")]
+    public Placeable.PlaceableType pType;
+    public GameObject associatedPrefab;
+    public GameObject alternatePrefab;
+    
+
+    
+    
+}
